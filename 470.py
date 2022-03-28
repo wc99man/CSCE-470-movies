a72c8f312399cf8a7bce148fc7760d899c51cbed
hari2907 <102490857+hari2907@users.noreply.github.com>
2022-03-28 00:26:50 -0500

    Updated data size to 5000

--- 470.py
+++ 470.py
@@ -2,12 +2,12 @@ import pandas as pd
 pd.options.display.width = 0
 
 
-name_basics_df = pd.read_csv('Data/name_basics/data.tsv', sep='\t', dtype=str, nrows=4000)
-title_basics_df = pd.read_csv('Data/title_basics/data.tsv', sep='\t', dtype={'startYear': 'int32'}, nrows=4000)
+name_basics_df = pd.read_csv('Data/name_basics/data.tsv', sep='\t', dtype=str, nrows=5000)
+title_basics_df = pd.read_csv('Data/title_basics/data.tsv', sep='\t', dtype={'startYear': 'int32'}, nrows=5000)
 title_basics_df = title_basics_df.rename(columns={'tconst':'titleId'})
-title_akas_df = pd.read_csv('Data/title_akas/data.tsv', sep='\t', dtype=str, nrows=4000)
-title_rating_df = pd.read_csv('Data/title_ratings/data.tsv', sep='\t', dtype=str, nrows=4000)
-title_crew_df = pd.read_csv('Data/title_crew/data.tsv', sep='\t', dtype=str, nrows=4000)
+title_akas_df = pd.read_csv('Data/title_akas/data.tsv', sep='\t', dtype=str, nrows=5000)
+title_rating_df = pd.read_csv('Data/title_ratings/data.tsv', sep='\t', dtype=str, nrows=5000)
+title_crew_df = pd.read_csv('Data/title_crew/data.tsv', sep='\t', dtype=str, nrows=5000)
 title_crew_df = title_crew_df.rename(columns={'tconst':'tconst1'})
 
 
@@ -22,9 +22,6 @@ title_merged = title_merged[title_merged['titleType']=="movie"]
 title_merged = pd.merge(left=title_merged, right = title_rating_df, left_on='titleId', right_on='tconst')
 title_merged = pd.merge(left=title_merged, right = title_crew_df, left_on='titleId', right_on='tconst1')
 title_merged=title_merged.drop(labels=['numVotes','tconst','tconst1','ordering','language','types','attributes','isOriginalTitle','titleType','primaryTitle','originalTitle','isAdult','endYear'],axis=1)
-# print(title.head(1000))
-# print(title_basics_df)
-# print(title_rating_df.head(20))
 
 #Converts title_merged df into dictionary (Format --> {movieName : {'titleId' : ..., 'Year': ..., etc.)
 title_dict = {title_merged['title'][index]:{} for index in title_merged.index}
