7599312b9542ac2a453d854a5aedd4e5a2b92d52
hari2907 <102490857+hari2907@users.noreply.github.com>
2022-04-11 17:19:07 -0500

    Application works with new dataset
    Added writers and directors to dictionary

--- app.py
+++ app.py
@@ -8,31 +8,16 @@ import math
 
 # pd.options.display.width = 0
 
-name_basics_df = pd.read_csv('Data/name_basics/data.tsv', sep='\t', dtype=str, nrows=5000)
-title_basics_df = pd.read_csv('Data/title_basics/data.tsv', sep='\t', dtype={'startYear': 'int32'}, nrows=5000)
-title_basics_df = title_basics_df.rename(columns={'tconst':'titleId'})
-title_basics_df = title_basics_df.replace(to_replace = "\\N", value = "0")
-title_basics_df = title_basics_df.astype({'runtimeMinutes' : 'int32' })
-title_akas_df = pd.read_csv('Data/title_akas/data.tsv', sep='\t', dtype=str, nrows=5000)
-title_rating_df = pd.read_csv('Data/title_ratings/data.tsv', sep='\t', dtype=str, nrows=5000)
-title_crew_df = pd.read_csv('Data/title_crew/data.tsv', sep='\t', dtype=str, nrows=5000)
-title_crew_df = title_crew_df.rename(columns={'tconst':'tconst1'})
-
-title = title_akas_df[title_akas_df["region"]=="US"]
-# title_basics_df = title_basics_df[title_basics_df["startYear"] > 2000]
-
-title_merged = pd.merge(left=title, right=title_basics_df, left_on='titleId',right_on='titleId')
-title_merged = title_merged[title_merged['titleType']=="movie"]
-title_merged = pd.merge(left=title_merged, right = title_rating_df, left_on='titleId', right_on='tconst')
-title_merged = pd.merge(left=title_merged, right = title_crew_df, left_on='titleId', right_on='tconst1')
-title_merged=title_merged.drop(labels=['numVotes','tconst','tconst1','ordering','language','types','attributes','isOriginalTitle','titleType','primaryTitle','originalTitle','isAdult','endYear'],axis=1)
+title_merged = pd.read_csv('Data/output.csv')
+title_merged['runtimeMinutes'] = title_merged['runtimeMinutes'].replace('\\N','0')
+title_merged['genres'] = title_merged['genres'].replace('\\N','None')
 
 #Converts title_merged df into dictionary (Format --> {movieName : {'titleId' : ..., 'Year': ..., etc.)
 title_dict = {title_merged['title'][index]:{} for index in title_merged.index}
 
 for index in title_merged.index:
     movieName = title_merged['title'][index]
-    title_dict[movieName] = {title_merged['titleId'][index] : 0, title_merged['startYear'][index] : 0, title_merged['runtimeMinutes'][index] : 0, title_merged['genres'][index] : 0, title_merged['averageRating'][index] : 0}
+    title_dict[movieName] = {title_merged['titleId'][index] : 0, title_merged['startYear'][index] : 0, title_merged['runtimeMinutes'][index] : 0, title_merged['genres'][index] : 0, title_merged['averageRating'][index] : 0, title_merged['directors'][index] : 0, title_merged['writers'][index] : 0}
 
 def predict(moviename, moviedict):  ##### we need the name of the movie so we can make a new vector for it
 
